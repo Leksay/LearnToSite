f95071a4eb72863196c250d45eb6ac55a6ac654a
Alexey <twogame@yandex.ru>
2018-09-30 15:02:15 +0300

    Add the VS solution

---
+++ Unity_State/Unity_State/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Unity_State.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
