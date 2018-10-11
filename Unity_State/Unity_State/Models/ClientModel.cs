17b1f39535cc57abe1d1650d2995c2a9bf76cd3f
Leksay <43336918+Leksay@users.noreply.github.com>
2018-10-12 00:23:21 +0300

    0.01

---
+++ Unity_State/Unity_State/Models/ClientModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unity_State.Models
+{
+    public class ClientModel
+    {
+        public enum Sex
+        {
+            Male, Famale
+        }
+        public string login;
+        public string password;
+        public Sex sex;
+        public string number;
+        public string email;
+
+    }
+}
